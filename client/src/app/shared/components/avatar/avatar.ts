59c38260d2c252ede9a31b26d773fe27834e836f
Randumb <78370387+RandumbGuy1@users.noreply.github.com>
2026-06-22 21:26:58 -0400

    Profile page finalization + gitignore avatars

--- client/src/app/shared/components/avatar/avatar.ts
+++ client/src/app/shared/components/avatar/avatar.ts
@@ -5,7 +5,6 @@ import { Observable } from 'rxjs/internal/Observable';
 import { AuthService } from '../../../core/auth/auth.service';
 import { UserStore } from '../../../core/store/user.store';
 import { AsyncPipe } from '@angular/common';
-import { UserService } from '../../../core/services/user.service';
 
 @Component({
   selector: 'app-avatar',
@@ -19,7 +18,7 @@ export class AvatarComponent {
   tier: string = '';
   profile$!: Observable<UserProfile | null>;
 
-  constructor(public router: Router, public userService: UserService, public userStore: UserStore, public auth: AuthService) {}
+  constructor(public router: Router, public userStore: UserStore, public auth: AuthService) {}
 
   ngOnInit(): void {
     let user = this.auth.getCurrentUser();
